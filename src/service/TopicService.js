842a39a7b88b009afc196d3f6b321c61ff4379dc
hyejinU <gpwls4306@g.skku.edu>
2022-11-06 09:44:12 +0900

    temp

--- src/service/TopicService.js
+++ src/service/TopicService.js
@@ -7,13 +7,14 @@ const TopicService = {
     try {
       const pool = sql.createPool(config);
       const connection = await pool.getConnection(async (conn) => conn);
-    
+
       try {
         await connection.beginTransaction();
         try {
-          result = await connection.query(
-            `insert into Untopic values(null, ?, ?, 0, default)`, [title, user_id]);
-        } catch (error) {return false;}
+          result = await connection.query(`insert into Untopic values(null, ?, ?, 0, default)`, [title, user_id]);
+        } catch (error) {
+          return false;
+        }
         await connection.commit();
       } catch (err) {
         await connection.rollback();
@@ -24,60 +25,46 @@ const TopicService = {
       }
 
       result = {
-        "result" : 'success'
-      }
+        result: 'success',
+      };
       return result;
     } catch (error) {
       result = {
-        "result" : 'fail'
-      }
+        result: 'fail',
+      };
       throw new Error(`USER/${error}`);
     }
   },
 
-  voteTopic : async (untopic_id, user_id) => {
+  voteTopic: async (untopic_id, user_id) => {
     let result = [];
     try {
       const pool = sql.createPool(config);
       const connection = await pool.getConnection(async (conn) => conn);
-      
+
       let isLiked = [];
-  
+
       try {
         await connection.beginTransaction();
-        try {          
-          isLiked = await connection.query(
-            `select * from Untopic_like where untopic_id = ? and user_id = ?`,
-            [untopic_id, user_id]
-          )
-      
-          
-          isLiked = isLiked[0].length
-          console.log(isLiked)
+        try {
+          isLiked = await connection.query(`select * from Untopic_like where untopic_id = ? and user_id = ?`, [untopic_id, user_id]);
 
-          if(isLiked == 0){
-            
-            result = await connection.query(
-              `insert into Untopic_like(untopic_id, user_id, regdata) values(?, ?, default)`, [untopic_id, user_id]);
-            await connection.query(
-              `update Untopic set likes = likes + 1 where untopic_id = ?`, 
-              [untopic_id]
-            );
+          isLiked = isLiked[0].length;
+          console.log(isLiked);
+
+          if (isLiked == 0) {
+            result = await connection.query(`insert into Untopic_like(untopic_id, user_id, regdata) values(?, ?, default)`, [untopic_id, user_id]);
+            await connection.query(`update Untopic set likes = likes + 1 where untopic_id = ?`, [untopic_id]);
             result = {
-              "result" : '투표 성공'
+              result: '투표 성공',
             };
-          }
-          else{
-            result = await connection.query(
-              `delete from Untopic_like where untopic_id = ? and user_id = ?`, [untopic_id, user_id]);
-      
-            await connection.query(
-              `update Untopic set likes = likes - 1 where untopic_id = ?`, 
-              [untopic_id]
-            );
+          } else {
+            result = await connection.query(`delete from Untopic_like where untopic_id = ? and user_id = ?`, [untopic_id, user_id]);
+
+            await connection.query(`update Untopic set likes = likes - 1 where untopic_id = ?`, [untopic_id]);
             result = {
-              "result" : '투표 취소'
-            }
+              result: '투표 취소',
+            };
           }
         } catch (error) {
           return false;
@@ -93,13 +80,13 @@ const TopicService = {
       return result;
     } catch (error) {
       result = {
-        "result" : 'fail'
-      }
+        result: 'fail',
+      };
       throw new Error(`USER/${error}`);
     }
   },
 
-  randomTopic : async () => {
+  randomTopic: async () => {
     let result = [];
     try {
       const pool = sql.createPool(config);
@@ -109,13 +96,12 @@ const TopicService = {
       try {
         await connection.beginTransaction();
         try {
-          topics = await connection.query(
-            `select * from Untopic order by rand() LIMIT 0, 5`);
+          topics = await connection.query(`select * from Untopic order by rand() LIMIT 0, 6`);
           topics = topics[0];
 
           result = {
-            "randomtopics" : topics,
-          }
+            randomtopics: topics,
+          };
         } catch (error) {
           return false;
         }
@@ -133,18 +119,16 @@ const TopicService = {
     }
   },
 
-  confirmTopic : async (title, endtime) => {
+  confirmTopic: async (title, endtime) => {
     try {
       const pool = sql.createPool(config);
       const connection = await pool.getConnection(async (conn) => conn);
       let result = [];
-    
+
       try {
         await connection.beginTransaction();
         try {
-          await connection.query(
-            `insert into Topic values(null, ?, default, ?)`, 
-            [title, endtime]);
+          await connection.query(`insert into Topic values(null, ?, default, ?)`, [title, endtime]);
         } catch (error) {
           return false;
         }
@@ -157,8 +141,8 @@ const TopicService = {
         pool.end();
       }
       result = {
-        "success" : 'success'
-      }
+        success: 'success',
+      };
       return result;
     } catch (error) {
       throw new Error(`USER/${error}`);
